8368217a0d81683e7ab3995d442dc20ede2237e2
pygm4li0n <z0r4lity@gmail.com>
2026-08-31 23:06:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@
     // ============================================================
     // Token Tracker (DexScreener) – using token address, shows name and logo
     // ============================================================
-    const TOKEN_ADDRESS = '3ZMNoxKjdybeyfKWnK8esKQGw42MCsBkMTzrfzzTyhbX';
+    const TOKEN_ADDRESS = '3C6nz382qU71aDiEvk5xsmJrzd5Ey6iNXdpTgrjVpump';
 
     async function updateTokenInfo() {
         const logoEl = document.getElementById('tokenLogo');
